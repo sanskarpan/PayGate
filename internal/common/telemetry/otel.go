13fb6951ea75e23842c718635008c242157db4d5
sanskarpan <sanskarpandey2004@gmail.com>
2026-04-30 13:46:57 +0530

    Add internal/common/telemetry/otel.go
    Refs #82

---
+++ internal/common/telemetry/otel.go
@@ -0,0 +1,39 @@
+package telemetry
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
+)
+
+func Init(ctx context.Context, service string) (func(context.Context) error, error) {
+	exporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+	if err != nil {
+		return nil, fmt.Errorf("create stdout otel exporter: %w", err)
+	}
+
+	res, err := resource.New(ctx, resource.WithAttributes(semconv.ServiceName(service)))
+	if err != nil {
+		return nil, fmt.Errorf("create otel resource: %w", err)
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(res),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+	return tp.Shutdown, nil
+}
+
+func WrapHTTP(handler http.Handler, operation string) http.Handler {
+	return otelhttp.NewHandler(handler, operation)
+}
