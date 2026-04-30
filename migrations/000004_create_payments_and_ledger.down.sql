e030abee640502bdf64428a05cda78ae58a925a4
sanskarpan <sanskarpandey2004@gmail.com>
2026-04-30 13:51:13 +0530

    Add migrations/000004_create_payments_and_ledger.down.sql
    Refs #131

---
+++ migrations/000004_create_payments_and_ledger.down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS paygate_ledger.ledger_entries;
+DROP TABLE IF EXISTS paygate_ledger.ledger_transactions;
+DROP TABLE IF EXISTS paygate_ledger.ledger_accounts;
+DROP TABLE IF EXISTS paygate_payments.payments;
+DROP TABLE IF EXISTS paygate_payments.payment_attempts;
+DROP SCHEMA IF EXISTS paygate_ledger;
+DROP SCHEMA IF EXISTS paygate_payments;
