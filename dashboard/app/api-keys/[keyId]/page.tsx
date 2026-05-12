d61c4be2f8f77c6e2eb5f625e94c206ace39c31c
sanskarpan <sanskarpandey2004@gmail.com>
2026-05-12 12:05:55 +0530

    feat(dashboard): promote api key detail route into a full operator page
    Refs #402

--- dashboard/app/api-keys/[keyId]/page.tsx
+++ dashboard/app/api-keys/[keyId]/page.tsx
@@ -1,5 +1,7 @@
-import { requireViewer } from "../../../lib/api";
-import IPAllowlistManager from "../../../components/ip-allowlist-manager";
+import { notFound } from "next/navigation";
+
+import { getAPIKeys, requireViewer } from "../../../lib/api";
+import APIKeyDetailManager from "../../../components/api-key-detail-manager";
 import { getApiBaseUrl } from "../../../lib/api";
 
 interface Props {
@@ -8,10 +10,10 @@ interface Props {
 
 export default async function APIKeyAllowlistPage({ params }: Props) {
   await requireViewer();
-  return (
-    <IPAllowlistManager
-      keyId={params.keyId}
-      apiBaseUrl={getApiBaseUrl()}
-    />
-  );
+  const keys = await getAPIKeys();
+  const key = keys.items.find((item) => item.id === params.keyId);
+  if (!key) {
+    notFound();
+  }
+  return <APIKeyDetailManager apiBaseUrl={getApiBaseUrl()} item={key} />;
 }
