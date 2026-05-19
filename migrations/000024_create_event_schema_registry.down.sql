ce6c69908186a18d394f7a7e92f6a2d521344829
sanskarpan <sanskarpandey2004@gmail.com>
2026-05-19 13:39:56 +0530

    Update migrations/000024_create_event_schema_registry.down.sql for #420

---
+++ migrations/000024_create_event_schema_registry.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS paygate_schema.schema_rollout_consumers;
+DROP TABLE IF EXISTS paygate_schema.schema_rollouts;
+DROP TABLE IF EXISTS paygate_schema.schema_compatibility_checks;
+DROP TABLE IF EXISTS paygate_schema.schema_versions;
+DROP TABLE IF EXISTS paygate_schema.event_schemas;
+DROP SCHEMA IF EXISTS paygate_schema;
