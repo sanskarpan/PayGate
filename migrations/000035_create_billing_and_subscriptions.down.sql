bdd2c579ac558eb75835775bbdb46b049147568e
sanskarpan <sanskarpandey2004@gmail.com>
2026-05-24 12:01:50 +0530

    [#470] Track migrations/000035_create_billing_and_subscriptions.down.sql

---
+++ migrations/000035_create_billing_and_subscriptions.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS paygate_billing.invoice_attempts;
+DROP TABLE IF EXISTS paygate_billing.invoices;
+DROP TABLE IF EXISTS paygate_billing.subscriptions;
+DROP TABLE IF EXISTS paygate_billing.customers;
+DROP SCHEMA IF EXISTS paygate_billing;
